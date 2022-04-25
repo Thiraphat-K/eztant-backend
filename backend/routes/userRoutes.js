e8257b90b8d5c90501f71e7af62601fe7423d0a3
naveeharn <naveeharn@hotmail.com>
2022-04-26 03:11:38 +0700

    update get to post get users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, updateUser, getMe, loginUser, getUsers } = require('../controllers/userControllers')
+const { registerUser, updateUser, getMe, loginUser, getUsers, createTranscript } = require('../controllers/userControllers')
 const { protect } = require('../middleware/authenMiddleware')
+const { validateTranscript } = require('../utils/validateTranscript')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.put('/update', protect, updateUser)
 router.get('/getme', protect ,getMe)
-router.get('/getusers', protect ,getUsers)
+router.post('/getusers', protect ,getUsers)
+router.post('/transcript', protect, validateTranscript, createTranscript)
 
 
 module.exports = router
