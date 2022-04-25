a42bdd663350d91f417bd3cdcb211ba7b4b0f40e
naveeharn <naveeharn@hotmail.com>
2022-04-25 15:27:38 +0700

    enable cors

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,14 @@ connectDatabase()
 
 const app = express()
 
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/recruit_post', require('./routes/recruitPostRoutes'))
@@ -19,6 +25,6 @@ app.use('/api/community', require('./routes/communityRoutes'))
 
 app.use(errorHandler)
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started on port ${port} : ${new Date().toLocaleString()}`);
 })
