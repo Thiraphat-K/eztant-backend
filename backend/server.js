d9ed7be2a5ea95a82d77594721f7b60d2822dc47
naveeharn <naveeharn@hotmail.com>
2022-04-26 16:14:57 +0700

    add transcript method

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const connectDatabase = require('./configuration/database')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const FileUp = require('express-fileupload');
+
 
 const cors = require('cors')
 
@@ -12,10 +14,10 @@ connectDatabase()
 const app = express()
 
 app.use(cors())
-
+app.use(FileUp());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+app.use(express.json({ limit: '2MB' }));
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/recruit_post', require('./routes/recruitPostRoutes'))
 app.use('/api/notification', require('./routes/notificationRoutes'))
