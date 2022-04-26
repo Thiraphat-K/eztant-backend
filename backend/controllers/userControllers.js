ce3601f05262a89b0f797cc2d1bd1b3f7856ab08
naveeharn <naveeharn@hotmail.com>
2022-04-26 20:44:36 +0700

    fix receiver_id field

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -192,7 +192,7 @@ const getMe = asyncHandler(async (req, res) => {
                 },
             ])
     }
-    user['notifications'] = await notificationModel.find({ owner_id: user._id }).select('-_id event_type description api_link is_watched createdAt').sort({ createdAt: -1 })
+    user['notifications'] = await notificationModel.find({ receiver_id: user._id }).select('-_id event_type description api_link is_watched createdAt').sort({ createdAt: -1 })
     res.status(200).json(user)
 })
 
