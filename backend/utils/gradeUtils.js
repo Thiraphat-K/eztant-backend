5038968e397cdbc44200cb8a4b236d2f5dfb7cff
naveeharn <naveeharn@hotmail.com>
2022-04-26 20:53:30 +0700

    add gradeUtils

---
+++ backend/utils/gradeUtils.js
@@ -0,0 +1,16 @@
+const gradeUitls = {
+    "A": 4,
+    "+B": 3.5,
+    "B": 3,
+    "+C": 2.5,
+    "C": 2,
+    "+D": 1.5,
+    "D": 1,
+    "F": 0,
+    "S": 4,
+    "U": 4
+}
+
+module.exports = {
+    gradeUitls
+}
