863a415b9bf2f3b880e934146297a55f5cb0eddf
naveeharn <naveeharn@hotmail.com>
2022-04-26 16:13:41 +0700

    add transcript path

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router()
 const { registerUser, updateUser, getMe, loginUser, getUsers, createTranscript } = require('../controllers/userControllers')
 const { protect } = require('../middleware/authenMiddleware')
+const { exportPDF } = require('../middleware/transcriptMiddleware')
 const { validateTranscript } = require('../utils/validateTranscript')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.put('/update', protect, updateUser)
-router.get('/getme', protect ,getMe)
-router.post('/getusers', protect ,getUsers)
-router.post('/transcript', protect, validateTranscript, createTranscript)
+router.get('/getme', protect, getMe)
+router.post('/getusers', protect, getUsers)
+router.post('/transcript', protect, exportPDF, createTranscript)
 
 
 module.exports = router
