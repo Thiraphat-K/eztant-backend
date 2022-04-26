205bc7021d73f12cefa03f482ea80ab76a93003a
naveeharn <naveeharn@hotmail.com>
2022-04-26 16:13:56 +0700

    add validate transcript

--- backend/utils/validateTranscript.js
+++ backend/utils/validateTranscript.js
@@ -1,12 +1,10 @@
-const validateTranscript = ({ text, info, metadata }) => {
-
+const validateTranscript = ( text, info, metadata ) => {
     if (metadata) return false;
     if (Object.keys(info).length > 5) return false;
     if (info.PDFFormatVersion !== "1.3") return false;
     if (info.Producer !== "FPDF 1.53") return false;
     if (!text) return false;
     if (!text.length) return false;
-
     return true;
 };
 
