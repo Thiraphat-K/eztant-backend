e61d7d267946433cb5a33b59dc2fef330af9e8d9
naveeharn <naveeharn@hotmail.com>
2022-04-28 02:48:30 +0700

    add aggregate attendance config

--- backend/configuration/aggregate_config.js
+++ backend/configuration/aggregate_config.js
@@ -60,4 +60,63 @@ const aggregate_config = [
     },
 ]
 
-module.exports = {aggregate_config}
+const aggregate_attendance_config = [
+    // match({ age: { $lt: 30 } }),
+    { $match: 
+        { section: 
+            { 
+                $lt: 300,
+            } 
+        } 
+    },
+    // {
+    //     $lookup: {
+    //         from: "usermodels",
+    //         localField: "owner_id",
+    //         foreignField: "_id",
+    //         as: "owner_id"
+    //     }
+    // },
+    // {
+    //     $unwind: '$owner_id'
+    // },
+    // {
+    //     $lookup: {
+    //         from: "schedulemodels",
+    //         localField: "schedules",
+    //         foreignField: "_id",
+    //         as: "schedules"
+    //     }
+    // },
+    // {
+    //     $lookup: {
+    //         from: "commentmodels",
+    //         localField: "comments",
+    //         foreignField: "_id",
+    //         as: "comments"
+    //     }
+    // },
+    // {
+    //     $group: '$section'
+    // },
+    // {
+    //     "$project": {
+    //         // "_id": 1,
+    //         // "owner_id": 1,
+    //         // "owner_id._id": 1,
+    //         // "owner_id.email": 1,
+    //         "owner_id.firstname": 1,
+    //         "owner_id.lastname": 1,
+    //         "owner_id.department": 1,
+    //         "owner_id.role": 1,
+    //         "owner_id.img_url": 1,
+    //         "section": 1,
+    //         "attend_date": 1,
+    //         "check_by_teacher": 1,
+    //         "evidence_url": 1,
+    //         "createdAt": 1,
+    //     }
+    // },
+]
+
+module.exports = { aggregate_config, aggregate_attendance_config }
