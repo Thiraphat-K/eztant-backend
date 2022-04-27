5cb9e6062b7c5a54d80b520d898a5e1a753939cf
naveeharn <naveeharn@hotmail.com>
2022-04-27 20:11:11 +0700

    add colors utils

---
+++ backend/utils/colorsUtils.js
@@ -0,0 +1,17 @@
+const colorsUtils = ['#FF6B6B',
+    '#FFD93D',
+    '#6BCB77',
+    '#4D96FF',
+    '#88E0EF',
+    '#069A8E',
+    '#FF85B3',
+    '#9FB4FF',
+    '#85C88A',
+    '#F76E11',
+    '#E5890A',
+    '#9D5C0D',
+]
+
+module.exports = {
+    colorsUtils
+}
