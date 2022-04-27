dcd266702e9ad5daaf84ac34fb0bf13c85590f26
naveeharn <naveeharn@hotmail.com>
2022-04-27 16:52:48 +0700

    update validate accept post

--- backend/middleware/validatePosts.js
+++ backend/middleware/validatePosts.js
@@ -58,30 +58,27 @@ const validate_requested_post = asyncHandler(async (req, res, next) => {
 const validate_accepted_post = asyncHandler(async (req, res, next) => {
     const user = req.user
     // check role user
-    if (user.role == 'teacher' && user.role !== 'student') {
+    if (user.role == 'student' && user.role !== 'teacher') {
         res.status(400)
-        throw new Error('สำหรับนักศึกษาเท่านั้น')
+        throw new Error('สำหรับอาจารย์เท่านั้นเท่านั้น')
     }
-    if (!req.params['_id']) {
+    if (!req.params['schedule_id']) {
         res.status(401)
         throw new Error('schedule_id not found')
     }
-    const schedule = await scheduleModel.findById(req.params['_id'])
-
+    const schedule = await scheduleModel.findById(req.params['schedule_id'])
     let requested_schedule_time = {
         day: schedule.day,
         time_from: schedule.time_from.split(':')[0] * 60 + schedule.time_from.split(':')[1] * 1,
         time_to: schedule.time_to.split(':')[0] * 60 + schedule.time_from.split(':')[1] * 1,
     }
-    const recruit_post = await recruitPostModel.findById(schedule.recruit_post_id)
-    const accepted_requested = await scheduleModel.findOne({ recruit_post_id: recruit_post._id, accepted: user._id })
-    if (accepted_requested) {
-        res.status(400)
-        throw new Error('ไม่สามารถยกเลิกการสมัครได้ เนื่องจากอาจารย์ผู้สอนตอบรับการสมัครแล้ว')
-    }
     // const recruit_post = await recruitPostModel.findById(schedule.recruit_post_id)
-    const accepted_schedules = await scheduleModel.find({ accepted: user._id })
-    // console.log(accepted_schedules);
+    // const accepted_requested = await scheduleModel.findOne({ recruit_post_id: recruit_post._id, accepted: req.params['user_id'] })
+    // if (accepted_requested) {
+    //     res.status(400)
+    //     throw new Error('ไม่สามารถยกเลิกการสมัครได้ เนื่องจากอาจารย์ผู้สอนตอบรับการสมัครแล้ว')
+    // }
+    const accepted_schedules = await scheduleModel.find({ accepted: req.params['user_id'] })
     accepted_schedules.forEach(accepted => {
         let accepted_schedule_time = {
             day: accepted.day,
@@ -99,12 +96,10 @@ const validate_accepted_post = asyncHandler(async (req, res, next) => {
             if (before.time_to > after.time_from) {
                 // const post = await recruitPostModel.findById(accepted.recruit_post_id)
                 res.status(400)
-                throw new Error(`ไม่สามารถส่งคำขอการเป็น TA วิชานี้ได้ เนื่องจากทับซ้อนตารางเวลาการปฏิงาน TA ของวิชา`)
+                throw new Error(`ไม่สามารถยืนยันคำขอการเป็น TA วิชานี้ได้ เนื่องจากทับซ้อนตารางเวลาการปฏิงาน TA ของวิชาอื่น`)
             }
         }
     });
-
-    // const accepted_recruit_post = await recruitPostModel.find({ _id: accepted_schedules })
 
     next()
 })
