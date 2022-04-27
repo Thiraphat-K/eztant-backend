0f1b171a7ff76e0e4cdc832d89fab7e2c53f1332
naveeharn <naveeharn@hotmail.com>
2022-04-27 16:10:22 +0700

    update function name

--- backend/routes/recruitPostRoutes.js
+++ backend/routes/recruitPostRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router()
 const { setRecruitPost, likeRecruitPost, requestedRecruitPost, acceptedRecruitPost, commentRecruitPost, getRecruitPosts, getRecruitPost, recommendRecruitPost, deleteRecruitPost } = require("../controllers/recruitPostControllers");
 const { protect } = require("../middleware/authenMiddleware");
-const { validate_requested } = require("../middleware/validateRequested");
+const { validate_requested_post } = require("../middleware/validatePosts");
 
 router.post('/getposts', protect, getRecruitPosts)
 router.get('/recommends', protect, recommendRecruitPost)
@@ -11,7 +11,7 @@ router.get('/:_id', protect, getRecruitPost)
 router.delete('/:_id/delete', protect, deleteRecruitPost)
 router.post('/:_id/like', protect, likeRecruitPost)
 router.post('/:_id/comment', protect, commentRecruitPost)
-router.post('/schedule/:_id/request', protect, validate_requested, requestedRecruitPost)
+router.post('/schedule/:_id/request', protect, validate_requested_post, requestedRecruitPost)
 router.post('/schedule/:schedule_id/accept/:user_id', protect, acceptedRecruitPost)
 
 module.exports = router
